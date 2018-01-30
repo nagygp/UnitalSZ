5d5364d3327c5218a70d1e6aa7aadfba6829d24f
Dávid Mezőfi <david.mezofi93@gmail.com>
2018-01-30 18:43:48 +0100

    Write down the documentation for fullpoints.gd

--- gap/fullpoints.gd
+++ gap/fullpoints.gd
@@ -1,14 +1,34 @@
 #! @Arguments U
-#! @Returns The list of records containing the triples <M>(b_1,b_2,P)</M>,
-#!   where <M>P</M> is a full point of <A>U</A> w.r.t. the blocks <M>b_1,b_2</M>.
-#! @Description
-#!   The point <M>P</M> is a full point of <A>U</A> w.r.t. the blocks <M>b_1,b_2</M>
-#!   if <M>P</M> is not contained in <M>b_1</M> or <M>b_2</M>, and, the projection
-#!   with center <M>P</M> from <M>b_1</M> to <M>b_2</M> is a well-defined bijection.
+#! @Returns The list of records containing the triples <A>(b1, b2, list)</A>,
+#! where any member of the list <A>list</A> is a full point of <A>U</A> w.r.t.
+#! the blocks <A>b1, b2</A>.
+#! @Description The point <M>P</M> is a full point of <A>U</A> w.r.t. the blocks
+#!   <A>b1,b2</A> if <M>P</M> is not contained in <A>b1</A> or <A>b2</A>, and,
+#!   the projection with center <M>P</M> from <A>b1</A> to <A>b2</A> is a
+#!   well-defined bijection.
 DeclareAttribute( "FullPointsOfUnital", IsAU_UnitalDesign );
-#! @Description
-#!   Insert documentation for you function here
+#! @Arguments U
+#! @Returns The list of records containing the triples <A>(ind_b1, ind_b2,
+#!   ind_list)</A>, where any member of the list <A>ind_list</A> is an index of a full
+#!   point of <A>U</A> w.r.t. the blocks corresponding to <A>ind_b1, ind_b2</A>.
+#! @Description The point corresponding to the index <A>P</A> is a full point of
+#!   <A>U</A> w.r.t. the blocks corresponding to the indices <A>ind_b1,
+#!   ind_b2</A> if <A>P</A> is not contained in <A>ind_b1</A> or <A>ind_b2</A>,
+#!   and, the projection with center <A>P</A> from <A>ind_b1</A> to
+#!   <A>ind_b2</A> is a well-defined bijection.
 DeclareAttribute( "FullPointsOfUnitalRepresentatives", IsAU_UnitalDesign );
-#! @Description
-#!   Insert documentation for you function here
+#! @Arguments U
+#! @Returns The list of records containing the quadruples <A>(ind_b1, ind_b2,
+#!   ind_list, perm_list)</A>, where any member of the list <A>ind_list</A> is
+#!   an index of a full point of <A>U</A> w.r.t. the blocks corresponding to
+#!   <A>ind_b1, ind_b2</A> and <A>perm_list</A> contains permutations of the
+#!   block <A>ind_b1</A> defined by projections between <A>ind_b1</A> and
+#!   <A>ind_b2</A> with centers from the list of full points <A>ind_list</A>.
+#! @Description The permutations in <A>perm_list</A> are constructed the
+#!   following way: first we project <A>ind_b1</A> to <A>ind_b2</A> with center
+#!   <A>P</A>, where <A>P</A> is the first element of the list of full points
+#!   <A>ind_list</A>, then we take all projections from <A>ind_b2</A> to
+#!   <A>ind_b1</A> with center <A>Q</A>, where <A>Q</A> ranges through
+#!   <A>ind_list</A>. All these projections define a permutation on
+#!   <A>ind_b1</A>, since the projections were well-defined bijections.
 DeclareAttribute( "GeneratorsOfProjectivityGroupsOfUnital", IsAU_UnitalDesign );
