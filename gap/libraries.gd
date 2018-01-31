21070ef3149399f147ffd66bd3e1cc7f58c756e4
Dávid Mezőfi <david.mezofi93@gmail.com>
2018-01-31 18:54:16 +0100

    Write down the documentation for libraries.gd

--- gap/libraries.gd
+++ gap/libraries.gd
@@ -5,21 +5,54 @@
 # Declarations
 #
 
+#! @Arguments nr, q, filename
+#! @Returns
+#!  The list of boolean incidence matrices of size <M>(q^3 + 1) \times q^2(q^2 -
+#!  q + 1)</M> read from <A>filename</A>.
 #! @Description
-#!   Insert documentation for you function here
+#!  The file <A>filename</A> must be gzipped and must contain <A>nr</A>
+#!  matrices of dimension mentioned above. The matrices must be 0-1 matrices
+#!  without any whitespace between the entries in one row and there must not be
+#!  any empty lines between matrices.
 DeclareGlobalFunction( "AU_ReadLibraryDataFromFiles" );
+#! @Arguments 
 #! @Description
-#!   Insert documentation for you function here
+#!   Reads in the incidence matrices from the libraries of unitals shipped with
+#!   the package.
 DeclareGlobalFunction( "AU_InitLibraryData" );
+#! @Arguments n
+#! @Returns
+#!   The <A>n</A>th (abstract) unital of order 3 of the unitals by Betten,
+#!   Betten and Tonchev.
 #! @Description
-#!   Insert documentation for you function here
+#!   In the paper Unitals and codes by Anton Betten, Dieter Betten and Vladimir
+#!   D.  Tonchev (Discrete Mathematics 267, 2003, 23-33.) 909 unitals of order 3
+#!   were constructed. The incidence matrices of these unitals are shipped with
+#!   the package.
 DeclareGlobalFunction( "AU_BBTUnital" );
+#! @Arguments n
+#! @Returns
+#!   The <A>n</A>th (abstract) unital of order 4 of the unitals by Krčadinac,
+#!   Nakić and Pavčević.
 #! @Description
-#!   Insert documentation for you function here
+#!   In the paper The Kramer-Mesner method with tactical decompositions: some
+#!   new unitals on 65 points by Vedran Krčadinac, Anamari Nakić and Mario Osvin
+#!   Pavčević (Journal of Combinatorial Designs 19, 2011, 290-303.) 1777 unitals
+#!   of order 4 were constructed. The incidence matrices of these unitals are
+#!   shipped with the package.
 DeclareGlobalFunction( "AU_KNPUnital" );
+#! @Arguments n
+#! @Returns
+#!   The <A>n</A>th (abstract) unital of order 3 of the unitals by Krčadinac.
 #! @Description
-#!   Insert documentation for you function here
+#!   In the paper Steiner 2-designs S(2, 4, 28) with nontrivial automorphisms by
+#!   Vedran Krčadinac (Glasnik Matematički, Vol. 37 (57), 2002, 259-268.) 4466
+#!   unitals of order 3 were constructed. This library contains all the unitals
+#!   of order 3 with nontrivial automorphism group. The incidence matrices of
+#!   these unitals are shipped with the package.
 DeclareGlobalFunction( "AU_KrcadinacUnital" );
+#! @Arguments 
 #! @Description
-#!   Insert documentation for you function here
+#!   The function prints the information about the available libraries of
+#!   unitals.
 DeclareGlobalFunction( "AU_LibraryInfo" );
